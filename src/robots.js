21f6112a49ddd0a8e71cd153183180f07c741c96
alifkrisnanda <alifkrisnanda023@gmail.com>
2020-03-07 11:59:51 +0700

    first

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 0,
+        name: 'Alif',
+        username: 'Shin',
+        email: 'alif@gmail.com'
+    },
+    {
+        id: 1,
+        name: 'Krisnanda',
+        username: "kris10",
+        email: 'krisnanda@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Nurhuda',
+        username: '02',
+        email: "002@gmail.com"
+    },
+    {
+        id: 3,
+        name: 'Rahardian',
+        username: '004',
+        email: 'shin@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Eloise Wong',
+        username: 'ewong',
+        email: 'ewong@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Kristy Bell',
+        username: 'kristi',
+        email: 'krisbel@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Robin Cruz',
+        username: 'cruz',
+        email: 'cruzz@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Nichole Joseph',
+        username: 'seph',
+        email: 'josep@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Alberto Potter',
+        username: 'albert',
+        email: 'albert@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Brendan Reynolds',
+        username: 'kent',
+        email: 'brakent@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Carl Hayes',
+        username: 'johnson',
+        email: 'cj@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Claire Woods',
+        username: 'redfield',
+        email: 'claire@gmail.com'
+    }
+];
